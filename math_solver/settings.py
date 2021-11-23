6c01da78143d41be67dff3fc7fb0e7c33aa0eae0
Rafael Rain <RafaelrainBR@outlook.com>
2021-11-22 21:31:08 -0300

    chore: fix allowed hosts

--- math_solver/settings.py
+++ math_solver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@$2f136fh!s+a5bsl(s5#tskwjdbigo$yq7z=7^@%_u@n&%u3j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
