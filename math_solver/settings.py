5c31991f3eeab306e67e5677ba0b0d5acb5f3fc4
Rafael Rain <RafaelrainBR@outlook.com>
2021-11-22 21:28:49 -0300

    fix: allow all hosts

--- math_solver/settings.py
+++ math_solver/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@$2f136fh!s+a5bsl(s5#tskwjdbigo$yq7z=7^@%_u@n&%u3j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
